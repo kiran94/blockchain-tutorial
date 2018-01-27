1552de9787a7af303f03dd5e43cbe40e33b159a1
Kiran94 <kiran_patel94@hotmail.com>
2018-01-27 13:01:24 +0000

    Implemented the new_transaction method so that when the details of a transaction are passed, they are added to the current transaction member and the next block to be mined is returned

--- blockchain.py
+++ blockchain.py
@@ -24,11 +24,24 @@ class Blockchain(object):
         '''
         pass
 
-    def new_transaction(self):
+    def new_transaction(self, sender, recipient, amount):
         '''
-            Adds a new transaction to the list of transactions.
+            Creates a new transaction to go to the next mined block.
+
+            :param sender: <str> Address of the sender
+            :param recipient: <str> Address of the recipient
+            :param amount: <int> Amount to send
+            :return: <int> The index of the block that will hold this transaction.
         '''
-        pass
+
+        self.current_transactions.append(
+            {
+                'sender' : sender,
+                'recipient' : recipient,
+                'amount' : amount
+            })
+
+        return self.last_block["index"] + 1
 
     @staticmethod
     def hash(block):
