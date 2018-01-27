5e48d3772264c8c12a3f2f6ab86b4fa0bbb019a1
Kiran94 <kiran_patel94@hotmail.com>
2018-01-27 17:27:44 +0000

    Implemeneted the new transaction endpoint

--- server.py
+++ server.py
@@ -4,8 +4,7 @@
 
 from uuid import uuid4
 
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
 
 from blockchain import Blockchain
 
@@ -28,9 +27,23 @@ def mine():
 @app.route('/transactions/new', methods=['GET'])
 def new_transactions():
     '''
-        Add a new transaction
+        Takes a JSON transaction request and adds it to the BlockChain. 
     '''
-    return "mine"
+    values = request.get_json()
+
+    # Check if the incoming request json has all the required fields. 
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Mak the new transaction and get the block it will be added too.
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    # Return a json message. 
+    response = { 'message' : f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
 
 @app.route('/chain', methods=['GET'])
 def chain():
